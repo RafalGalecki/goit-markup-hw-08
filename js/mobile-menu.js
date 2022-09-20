5d49df48146c8353bbdbd4a16cb51030a36c0a2d
Rafał Gałecki <darthunter@gmail.com>
2022-09-20 21:36:45 +0200

    Mobilne menu wstawione
    skrypt, tagi i style
    blok: mobile__menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
-  const mobileMenu = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const mobileMenu = document.querySelector(".mobile__menu");
+  const openMenuBtn = document.querySelector(".mobile__open");
+  const closeMenuBtn = document.querySelector(".mobile__close");
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
